8f415cf140d16c4e1eeafb414d8ceed1229974e9
Eki Susanto <eq.susanto@gmail.com>
2026-07-28 11:11:45 +0700

    update class and create new page

--- src/components/SecWork.jsx
+++ src/components/SecWork.jsx
@@ -14,8 +14,8 @@ function Portfolio({ title, subtitle, description, image, linkWork, label, image
 					<div className='self-center'>
 						<h3 className='text-[#665B53] text-center uppercase text-sm mb-3'>{subtitle}</h3>
 						<h2 className='text-center uppercase font-semibold text-4xl lg:text-5xl mb-4'>{title}</h2>
-						<p className='max-w-[466px] text-center text-base leading-[1.8] text-secondary-foreground/70 mx-auto mb-4'>{description}</p>
-						<a className='btn-outline inline-flex text-sm items-center gap-x-3 mt-2 font-semibold uppercase px-6 pt-[9px] pb-2' href={linkWork} target="_blank" rel="noopener noreferrer">
+						<p className='max-w-md text-center text-base leading-[1.8] text-secondary-foreground/70 mx-auto mb-4'>{description}</p>
+						<a className='btn-outline inline-flex text-sm items-center gap-x-3 mt-2 font-semibold uppercase px-6 pt-2.5 pb-2.5' href={linkWork} target="_blank" rel="noopener noreferrer">
 							<span className='self-center'>View Case Study</span>
 							<svg className='self-center' width='9' height='9' viewBox='0 0 7 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
 								<path d='M0.5 6.5L6.5 0.5M6.5 5.9V0.5H1.1' stroke='#333333' strokeLinecap='round' strokeLinejoin='round' />
@@ -23,7 +23,7 @@ function Portfolio({ title, subtitle, description, image, linkWork, label, image
 						</a>
 					</div>
 				</div>
-				<div style={{ order: imageOrder }} className='w-full h-full min-h-[350px] md:min-h-full relative'>
+				<div style={{ order: imageOrder }} className='w-full h-full min-h-87.5 md:min-h-full relative'>
 					<img className='w-full h-full object-cover' src={image} alt={title} />
 					<div className='absolute bottom-8 right-8'>
 						<ul className='flex gap-3'>
---
+++ src/pages/Work.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <div>
+            <h1>Work Page</h1>
+            <p>Welcome to the about page!</p>
+        </div>
+    );
+};
+
+export default Work;
