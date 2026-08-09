1a75a39973c2370ed7244ac7138a4474e69e8cd6
Eki Susanto <eq.susanto@gmail.com>
2026-08-09 22:22:11 +0700

    update other project

--- src/pages/WorkDetail.jsx
+++ src/pages/WorkDetail.jsx
@@ -167,12 +167,12 @@ function WorkDetail() {
                                     <div className='absolute inset-0 bg-linear-to-t from-[#0d0a06]/70 to-transparent' />
                                     <div className='absolute bottom-8 left-8 right-8'>
                                         <div className='container mx-auto'>
-                                            <div className='flex items-end justify-between'>
+                                            <div className='flex items-end justify-between mb-3'>
                                                 <div>
                                                     <ul className='flex items-center gap-4 mb-4'>
                                                         <li className='text-white uppercase text-xs glass font-hanken font-semibold rounded-full px-4 py-1 inline-block'>{nextProject.subtitle}</li>
                                                     </ul>
-                                                    <h2 className='text-white uppercase font-semibold text-xl lg:text-2xl mb-6'>{nextProject.title}</h2>
+                                                    <h2 className='text-white uppercase font-semibold text-xl lg:text-2xl leading-[1.4]'>{nextProject.title}</h2>
                                                 </div>
                                                 <div className="w-12 h-12 border border-white rounded-full flex items-center justify-center group-hover:bg-white/20 transition-colors duration-300 shrink-0 ml-4">
                                                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
